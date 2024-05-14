98b81dceeca9960690fef702b8fd28b8db436821
Diegoacs2004 <126615170+Diegoacs2004@users.noreply.github.com>
2024-05-13 22:41:02 -0500

    nav, header, home

---
+++ header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/header.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-scroll fixed-top shadow-0 border-bottom border-dark">
+        <div class="container">
+            <a class="navbar-brand" href="#!"><i class="fab fa-mdb fa-4x"></i></a>
+            <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fas fa-bars"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="./Vista/inicio.php">Inicio de Sesión</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./Vista/inicio.php">Crear Cuenta</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#!">Resultados</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#!">Eventos</a>
+                    </li>
+                    <button type="button" class="btn btn-dark ms-3">Mi cuenta</button>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</body>
+
+</html>
