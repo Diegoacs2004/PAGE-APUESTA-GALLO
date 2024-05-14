17a204ce67cf601519c642f06b0ad9f202ad25c2
DenisM. <68979805+MyC22@users.noreply.github.com>
2024-05-13 21:58:43 -0500

    formulario login

---
+++ Vista/loginVista.php
@@ -0,0 +1,29 @@
+<?php include('../header.php'); ?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="../CSS/login.css"> <!-- Enlazar el archivo CSS -->
+</head>
+
+<body>
+    <form class="container-registro">
+        <div class="title-init">
+            <h1>INICIAR SESION</h1>
+        </div>
+        <div class="container-inputs">
+            <input type="text" placeholder="Nombre / Correo:">
+            <input type="text" placeholder="Contraseña:">
+        </div>
+        <div>
+            <button>REGISTRARSE</button>
+        </div>
+        <p>¿Olvidaste tu contraseña?</p>
+    </form>
+</body>
+
+
+
+
+<?php include('../footer.php'); ?>
