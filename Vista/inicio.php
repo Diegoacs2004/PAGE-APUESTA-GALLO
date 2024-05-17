e7c5602e6e681e813a882112ea58e0405d35cfb6
marccec3 <marcceloc3@gmail.com>
2024-05-17 17:53:05 -0500

    comprargallo
    tarjetas

--- Vista/inicio.php
+++ Vista/inicio.php
@@ -7,6 +7,10 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/inicio.css">
+<<<<<<< Updated upstream
+=======
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+>>>>>>> Stashed changes
     <script src="./js/inicio.js"></script>
 </head>
 
@@ -14,14 +18,24 @@
     <?php require_once "../header.php"; ?>
     <!-- Carousel wrapper -->
     <div id="slide">
+<<<<<<< Updated upstream
         <div class="slide-inner">
+=======
+        <div class="slide-inner py-3">
+>>>>>>> Stashed changes
             <input class="slide-open" type="radio" id="slide-1" name="slide" aria-hidden="true" hidden="" checked="checked">
             <div class="slide-item">
                 <img src="img/gallo_evento_1.jpg" style="width: 70%; height: 100%;">
             </div>
             <input class="slide-open" type="radio" id="slide-2" name="slide" aria-hidden="true" hidden="">
             <div class="slide-item">
+<<<<<<< Updated upstream
                 <img src="https://www.migueltroyano.com/wp-content/uploads/2020/09/postgres_copy.png">
+=======
+                <video class="h-100 w-100 onlypc" autoplay="" loop="" muted="" playsinline="">
+                    <source type="video/mp4" src="https://www.cockfight.pe/manager/assets/sources/Peru-home-deskop.mp4?v=1.0">
+                </video>
+>>>>>>> Stashed changes
             </div>
             <input class="slide-open" type="radio" id="slide-3" name="slide" aria-hidden="true" hidden="">
             <div class="slide-item">
@@ -46,6 +60,42 @@
             </ol>
         </div>
     </div>
+<<<<<<< Updated upstream
+=======
+
+    <section>
+        <div class="container">
+            <div class="row content">
+                <div class="col-12 col-md-4 mb-4">
+                    <div class="card border-2">
+                        <img src="https://media.istockphoto.com/id/626045422/es/foto/dos-del-spreadon-de-la-lucha-del-martillo-en-la-granja.jpg?s=612x612&w=0&k=20&c=UueJrD5TuofWXCvtWTpmo1BZgnIYTOPOQl6iP3TFpX4=" class="card-img-top" alt="Eventos">
+                        <div class="card-body contenido text-center">
+                            <h6 class="card-title">Eventos <span class="material-symbols-outlined" style="vertical-align: middle;">arrow_forward</span> </h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-4 mb-4">
+                    <div class="card border-2">
+                        <img src="https://media.istockphoto.com/id/1980207106/es/foto/young-man-watching-soccer-game-at-home.jpg?s=612x612&w=0&k=20&c=p5RHiSVhgCpBWEDmW985Wfe5ZSTF76tdICH-ZY6z9JA=" class="card-img-top" alt="Eventos">
+                        <div class="card-body contenido text-center">
+                            <h6 class="card-title">En Vivo <span class="material-symbols-outlined" style="vertical-align: middle;">arrow_forward</span> </h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-4 mb-4">
+                    <div class="card border-2">
+                        <img src="https://media.istockphoto.com/id/1361718100/es/foto/hermoso-gallo-de-pie-en-la-hierba-en-un-fondo-verde-de-naturaleza-borrosa.jpg?s=612x612&w=0&k=20&c=-L-WR9O9USjxQvsUySE5v6Cdre6wlYnur7lO9Pls0Wc=" class="card-img-top" alt="Eventos">
+                        <div class="card-body contenido text-center">
+                            <h6 class="card-title">Compra tu gallo <span class="material-symbols-outlined" style="vertical-align: middle;">arrow_forward</span> </h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+>>>>>>> Stashed changes
 </body>
 <?php
 require_once "../footer.php"
--- header.php
+++ header.php
@@ -4,49 +4,43 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Radio+Canada+Big:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/header.css">
     <title>Document</title>
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                    </li>
-                </ul>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
+    <div class="contenedor-total px-5">
+        <header class="header py-3">
+            <div class="row flex-nowrap justify-content-between align-items-center">
+                <div class="col-4 pt-1">
+                    <a class="link-secondary icono_lupa" href="#" aria-label="Search">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="mx-3" role="img" viewBox="0 0 24 24">
+                            <title>Search</title>
+                            <circle cx="10.5" cy="10.5" r="7.5"></circle>
+                            <path d="M21 21l-5.2-5.2"></path>
+                        </svg>
+                    </a>
+                </div>
+                <div class="col-4 text-center">
+                    <a><img src="https://yostar.store/cdn/shop/files/logo_c812fe9d-3714-4539-a7bb-d618deadfb21.png?v=1661853019&width=500" width="500" height="120" alt=""></a>
+                </div>
+                <div class="col-4 d-flex justify-content-end align-items-center  icono_logeo">
+                    <a class="btn btn-sm btn-outline-secondary" href="#">Sign up</a>
+                </div>
             </div>
+        </header>
+        <div class="nav-scroller py-1 contenedor-links">
+            <nav class="nav d-flex justify-content-between">
+                <a class="p-2 link-secondary" href="#">Eventos</a>
+                <a class="p-2 link-secondary" href="#">En Vivo</a>
+                <a class="p-2 link-secondary" href="#">Gallos</a>
+                <a class="p-2 link-secondary" href="#">Apuestas</a>
+                <a class="p-2 link-secondary" href="#">Acerca de</a>
+                <a class="p-2 link-secondary" href="#">Membresias</a>
+            </nav>
         </div>
-    </nav>
-
-</body>
-
-</html>
+    </div>
