7de2171c25a9ccd584e37896f0930a2c49d3a6ca
marccec3 <marcceloc3@gmail.com>
2024-05-17 17:58:39 -0500

    comprargallo
    tarjetas

--- Vista/inicio.php
+++ Vista/inicio.php
@@ -7,10 +7,7 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/inicio.css">
-<<<<<<< Updated upstream
-=======
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
->>>>>>> Stashed changes
     <script src="./js/inicio.js"></script>
 </head>
 
@@ -18,24 +15,16 @@
     <?php require_once "../header.php"; ?>
     <!-- Carousel wrapper -->
     <div id="slide">
-<<<<<<< Updated upstream
-        <div class="slide-inner">
-=======
         <div class="slide-inner py-3">
->>>>>>> Stashed changes
             <input class="slide-open" type="radio" id="slide-1" name="slide" aria-hidden="true" hidden="" checked="checked">
             <div class="slide-item">
                 <img src="img/gallo_evento_1.jpg" style="width: 70%; height: 100%;">
             </div>
             <input class="slide-open" type="radio" id="slide-2" name="slide" aria-hidden="true" hidden="">
             <div class="slide-item">
-<<<<<<< Updated upstream
-                <img src="https://www.migueltroyano.com/wp-content/uploads/2020/09/postgres_copy.png">
-=======
                 <video class="h-100 w-100 onlypc" autoplay="" loop="" muted="" playsinline="">
                     <source type="video/mp4" src="https://www.cockfight.pe/manager/assets/sources/Peru-home-deskop.mp4?v=1.0">
                 </video>
->>>>>>> Stashed changes
             </div>
             <input class="slide-open" type="radio" id="slide-3" name="slide" aria-hidden="true" hidden="">
             <div class="slide-item">
@@ -60,8 +49,6 @@
             </ol>
         </div>
     </div>
-<<<<<<< Updated upstream
-=======
 
     <section>
         <div class="container">
@@ -95,7 +82,6 @@
     </section>
 
 
->>>>>>> Stashed changes
 </body>
 <?php
 require_once "../footer.php"
