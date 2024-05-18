4b4197cb2c1773db4bb5046be85cd20076b5e802
DenisM. <68979805+MyC22@users.noreply.github.com>
2024-05-18 13:03:32 -0500

    actualizacion cartas

--- Vista/inicio.php
+++ Vista/inicio.php
@@ -51,35 +51,21 @@
     </div>
 
     <section>
-        <div class="container">
-            <div class="row content">
-                <div class="col-12 col-md-4 mb-4">
-                    <div class="card border-2">
-                        <img src="https://media.istockphoto.com/id/626045422/es/foto/dos-del-spreadon-de-la-lucha-del-martillo-en-la-granja.jpg?s=612x612&w=0&k=20&c=UueJrD5TuofWXCvtWTpmo1BZgnIYTOPOQl6iP3TFpX4=" class="card-img-top" alt="Eventos">
-                        <div class="card-body contenido text-center">
-                            <h6 class="card-title">Eventos <span class="material-symbols-outlined" style="vertical-align: middle;">arrow_forward</span> </h6>
-                        </div>
-                    </div>
+        <div class="container pb-5">
+            <div class="contenedor-cards">
+                <div class="card card-orange">
+                    Tarjeta Brillante
                 </div>
-                <div class="col-12 col-md-4 mb-4">
-                    <div class="card border-2">
-                        <img src="https://media.istockphoto.com/id/1980207106/es/foto/young-man-watching-soccer-game-at-home.jpg?s=612x612&w=0&k=20&c=p5RHiSVhgCpBWEDmW985Wfe5ZSTF76tdICH-ZY6z9JA=" class="card-img-top" alt="Eventos">
-                        <div class="card-body contenido text-center">
-                            <h6 class="card-title">En Vivo <span class="material-symbols-outlined" style="vertical-align: middle;">arrow_forward</span> </h6>
-                        </div>
-                    </div>
+                <div class="card card-blue">
+                    Tarjeta Brillante
                 </div>
-                <div class="col-12 col-md-4 mb-4">
-                    <div class="card border-2">
-                        <img src="https://media.istockphoto.com/id/1361718100/es/foto/hermoso-gallo-de-pie-en-la-hierba-en-un-fondo-verde-de-naturaleza-borrosa.jpg?s=612x612&w=0&k=20&c=-L-WR9O9USjxQvsUySE5v6Cdre6wlYnur7lO9Pls0Wc=" class="card-img-top" alt="Eventos">
-                        <div class="card-body contenido text-center">
-                            <h6 class="card-title">Compra tu gallo <span class="material-symbols-outlined" style="vertical-align: middle;">arrow_forward</span> </h6>
-                        </div>
-                    </div>
+                <div class="card card-green">
+                    Tarjeta Brillante
                 </div>
             </div>
         </div>
     </section>
+
 
 
 </body>
