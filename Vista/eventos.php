8a226ccf93c278ba82ca9066ee137dd04b9535e9
Diegoacs2004 <126615170+Diegoacs2004@users.noreply.github.com>
2024-05-20 10:53:53 -0500

    Diego: Eventos css-vista

--- Vista/eventos.php
+++ Vista/eventos.php
@@ -10,45 +10,32 @@
 
 <body>
     <?php require_once "../header.php"; ?>
+
+
     <div class="contenedor-principal">
         <div class="row contenedor-eventos">
-            <div class="col-md-8 mb-5">
-                <div class="row">
-                    <div class="col">
-                        <h2 class="">MEJORES EVENTOS EN VIVO</h2>
+
+            <div class="row contenedor-title align-items-center">
+                <div class="col-md-6 mb-5">
+                    <div class="row">
+                        <div class="col">
+                            <h2 class="">MEJORES EVENTOS <span>EN VIVO</span></h2>
+                        </div>
                     </div>
                 </div>
-            </div>
-
-            <div class="col-md-6 mb-5">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="row-auto align-items-center">
-                            <div class="row">
-                                <h3><span>11</span> de Mayo</h3>
-                                <h3>15:00 PM - 18:00 PM</h3>
-                            </div>
-                            <div class="row">
-                                <div class="row justify-content-center">
-                                    <div class="col-auto">Gallo Diego</div>
-                                    <div class="col-auto"><img src="./img/gallo_box_1.png" alt="" style="width: 60px;"></div>
-                                    <div class="col-auto">
-                                        <h2>VS</h2>
-                                    </div>
-                                    <div class="col-auto"><img src="./img/gallo_box_1.png" alt="" style="width: 60px;"></div>
-                                    <div class="col-auto">Gallo Marcelo</div>
-                                </div>
-                            </div>
-                            <div class="row justify-content-center">
-                                <h3>Semifinales - Campo Deportivo</h3>
-                                <button style=" width: 30%;">Ver en vivo</button>
-                            </div>
+                <div class="col-md-6 mb-5">
+                    <div class="row">
+                        <!-- <div class="col"></div> -->
+                        <div class="col">
+                            <button class="btn-gallo">
+                                <h4 class="">VER GALLOS </h4>
+                            </button>
                         </div>
                     </div>
                 </div>
             </div>
 
-            <div class="col-md-12 mb-5">
+            <div class="col-md-8 mb-5">
                 <div class="card">
                     <div class="card-body">
                         <div class="row align-items-center">
@@ -59,11 +46,11 @@
                             <div class="col">
                                 <div class="row align-items-center">
                                     <div class="col">Gallo Diego</div>
-                                    <div class="col"><img src="./img/gallo_box_1.png" alt="" style="width: 60px;"></div>
+                                    <div class="row justify-content-center"><img src="./img/gallo_box_1.png" alt="" style="width: 100px;"></div>
                                     <div class="col">
                                         <h2>VS</h2>
                                     </div>
-                                    <div class="col"><img src="./img/gallo_box_1.png" alt="" style="width: 60px;"></div>
+                                    <div class="row justify-content-center"><img src="./img/gallo_box_1.png" alt="" style="width: 100px;"></div>
                                     <div class="col">Gallo Marcelo</div>
                                 </div>
                             </div>
@@ -76,7 +63,7 @@
                 </div>
             </div>
 
-            <div class="col-md-10 mb-5">
+            <div class="col-md-8 mb-5">
                 <div class="card">
                     <div class="card-body">
                         <div class="row align-items-center">
@@ -86,54 +73,34 @@
                             </div>
                             <div class="col">
                                 <div class="row align-items-center">
-                                    <div class="col"><img src="./img/gallo_box_1.png" alt="" style="width: 60px;"></div>
+                                    <div class="col">Gallo Diego</div>
+                                    <div class="row justify-content-center"><img src="./img/gallo_box_1.png" alt="" style="width: 100px;"></div>
                                     <div class="col">
                                         <h2>VS</h2>
                                     </div>
-                                    <div class="col"><img src="./img/gallo_box_1.png" alt="" style="width: 60px;"></div>
+                                    <div class="row justify-content-center"><img src="./img/gallo_box_1.png" alt="" style="width: 100px;"></div>
+                                    <div class="col">Gallo Marcelo</div>
                                 </div>
-
                             </div>
                             <div class="col">
                                 <h3>Semifinales - Campo Deportivo</h3>
                                 <button>Ver en vivo</button>
                             </div>
                         </div>
-
                     </div>
                 </div>
             </div>
 
-            <div class="col-md-10 mb-5">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="row align-items-center">
-                            <div class="col">
-                                <h3><span>11</span> de Mayo</h3>
-                                <h3>15:00 PM - 18:00 PM</h3>
-                            </div>
-                            <div class="col">
-                                <div class="row align-items-center">
-                                    <div class="col"><img src="./img/gallo_box_1.png" alt="" style="width: 60px;"></div>
-                                    <div class="col">
-                                        <h2>VS</h2>
-                                    </div>
-                                    <div class="col"><img src="./img/gallo_box_1.png" alt="" style="width: 60px;"></div>
-                                </div>
 
-                            </div>
-                            <div class="col">
-                                <h3>Semifinales - Campo Deportivo</h3>
-                                <button>Ver en vivo</button>
-                            </div>
-                        </div>
 
-                    </div>
-                </div>
-            </div>
+
 
         </div>
     </div>
+
+    <?php
+    require_once "../footer.php"
+    ?>
 
 </body>
 
